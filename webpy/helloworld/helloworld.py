c6faaff91a1afb6fef56d35396fa6869da2385e9
fsg <fsg@fsg-ThinkPad-T430.(none)>
2015-04-17 00:45:32 +0800

    webpy - db connected

--- webpy/helloworld/helloworld.py
+++ webpy/helloworld/helloworld.py
@@ -1,11 +1,37 @@
 import web
 
-urls = ("/.*", "hello")
+render = web.template.render('templates/')
+
+# urls = ("/.*", "hello")
+urls = ('/', "hello",
+        '/show', 'show',
+        '/database', 'database')
+
 app = web.application(urls, globals())
 
+db = web.database(dbn='mysql', user='root', pw='123456', db='webpytest')
+
 class hello:
+    # def GET(self):
+    def GET(self, name):
+        # name = 'Bod'
+        return render.helloworld(name)
+        # return 'Hello, world china!'
+        
+        # i = web.input(name=None)
+        # return render.helloworld(i.name)
+
+class show:
     def GET(self):
-        return 'Hello, world!'
+        return  "show"
+
+
+class database:
+    def GET(self):
+        if db is not None:
+            return "database connect"
+        else:
+            return "datbase operation"
 
 if __name__ == "__main__":
     app.run()
